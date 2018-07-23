1cd7e5268979805339f80a65d0afb46c0659e162
Juan Salvador <salvadorcortesf@gmail.com>
2018-07-22 23:33:56 -0500

    Playgrounds y Proyecto (al parecer playgrounds no esta funcionando ya que se actualiza muy seguido)

---
+++ Semana1/Hello World.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var x = 1
+
+var y = 2
+
+var z = y + x
+
+var h = "Hola YY"
+
+var image = UIImage(named: "lol.jpg")
+
